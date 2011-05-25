e0d9f7c42306d5f76a7d182ec5cec0d2397025a6
Guilherme Gregio <guilherme.gregio@dlbca.com.br>
2011-05-25 15:37:26 -0300

    add instancia

---
+++ python/python-django/cap-06-instancia.py
@@ -0,0 +1,14 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# cap-05-instancia.py;
+
+class Pessoa(object):
+	def __init__(self, parametro):
+		print "Parametro", parametro
+		
+pessoa1 = Pessoa("Spam")
+
+pessoa2 = Pessoa("Eggs")
+
+print pessoa1
+print pessoa2
