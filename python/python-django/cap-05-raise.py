6f0f7ec09397ff32c8263ea73a34c83324b10b29
Guilherme Gregio <guilherme.gregio@dlbca.com.br>
2011-05-25 11:20:00 -0300

    raise

---
+++ python/python-django/cap-05-raise.py
@@ -0,0 +1,14 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# cap-05-raise.py;
+
+class DivisaoPorZero(Exception):
+	pass
+	
+def divisao(divisor, dividendo):
+	if not dividendo: # dividendo é zero
+		raise DivisaoPorZero("Divisao por zero.")
+	return divisor / dividendo
+	
+divisao(10,0)
+print divisao(10,2)
