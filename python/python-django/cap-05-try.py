19ae08d543254b892366b1ecaca97bf78918f5b1
Guilherme Gregio <guilherme.gregio@dlbca.com.br>
2011-05-25 11:15:01 -0300

    add try

---
+++ python/python-django/cap-05-try.py
@@ -0,0 +1,18 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# cap-05-try.py;
+
+def le_arquivo(arquivo):
+	try:
+		arq = open(arquivo)
+		try:
+			linha1 = arq.readline()
+			linha2 = arq.readline()
+		finally:
+			print "Erro ao ler conteudo. Fechando arquivo"
+			arq.close()
+	except Exception, e:
+		print "Erro ao abrir o arquivo. O erro ocorrido foi: "
+		print str(e)
+		
+le_arquivo("nao_existe.txt")
