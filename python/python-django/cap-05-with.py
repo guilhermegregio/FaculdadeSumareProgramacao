1721340b7092c828feba4d9e750fc6b64412c6d6
Guilherme Gregio <guilherme.gregio@dlbca.com.br>
2011-05-25 10:39:52 -0300

    add python-django/cap-05-with.py

---
+++ python/python-django/cap-05-with.py
@@ -0,0 +1,9 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# cap-05-with.py;
+
+from __future__ import with_statement 
+
+with open("in.txt","r") as arquivo:
+    for n, linha in enumerate(arquivo):
+        print "%05d: %s" % (n, linha.strip())
