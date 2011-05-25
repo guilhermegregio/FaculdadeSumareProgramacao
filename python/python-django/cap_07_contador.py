08bae781d18358291ba851d1fac63a0013d2a8bc
Guilherme Gregio <guilherme.gregio@dlbca.com.br>
2011-05-25 16:52:36 -0300

    add modulos contador globais

---
+++ python/python-django/cap_07_contador.py
@@ -0,0 +1,10 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# cap_07_contador.py;
+
+try:
+	contador += 1
+except:
+	contador = 0
+
+print "Numero de reloads: %s" % (contador)
---
+++ python/python-django/cap_07_modulo_globais.py
@@ -0,0 +1,13 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# cap_07_modulo_globais.py Atributos globais do módulo;
+"""
+cap_07_modulo_globais.py
+
+Essa é a documentação do módulo cap_07_modulo_globais.py
+"""
+
+print __name__
+
+if __name__ == "__main__":
+	print "Executando modulo..."
